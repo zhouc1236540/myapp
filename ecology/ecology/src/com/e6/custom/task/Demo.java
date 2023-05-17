a396c2e18932a93df898e51e41a04a2811feae86
scott <zhoucheng123654@live.com>
2023-05-17 18:09:48 +0800

    提交啦

---
+++ ecology/ecology/src/com/e6/custom/task/Demo.java
@@ -0,0 +1,17 @@
+package com.e6.custom.task;
+
+/**
+ * @author: 山毛榉
+ * @date : 2023-05-09 11:31
+ * @version: 1.0
+ */
+public class Demo {
+
+    public  String get(){
+
+        System.out.println("get");
+
+        return  null;
+
+    }
+}
